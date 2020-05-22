306222e0a1097686871fd25b7d1635c21f8fa0ce
SAYAN MAITY <sayanmaity007@gmail.com>
2020-05-22 13:47:47 +0530

    minor warning fixed 🤗

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "./footer.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -13,23 +12,23 @@ export default class footer extends Component {
       <div className="footer" data-aos="fade">
         <div className="footer_text_area">
           <div className="footer_text">
-            Made With <i class="fas fa-heart"></i>
+            Made With <i className="fas fa-heart"></i>
           </div>
           <div className="footer_link">
             <ul>
               <li>
                 <p onClick={_=>window.open("https://github.com/sayancoding")}>
-                  <i class="fab fa-github-alt"></i>
+                  <i className="fab fa-github-alt"></i>
                 </p>
               </li>
               <li>
                 <p onClick={_=>window.open("https://www.instagram.com/backward_space/")}>
-                  <i class="fab fa-instagram"></i>
+                  <i className="fab fa-instagram"></i>
                 </p>
               </li>
               <li>
                 <p onClick={_=>window.open("https://www.linkedin.com/in/sayan-maity-6316921a6/")}>
-                  <i class="fab fa-linkedin"></i>
+                  <i className="fab fa-linkedin"></i>
                 </p>
               </li>
             </ul>
--- src/components/workCard/workCard.jsx
+++ src/components/workCard/workCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./workCard.css";
-import { Link } from "react-router-dom";
 
 export default function workCard(props) {
   return (
